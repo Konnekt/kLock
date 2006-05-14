ef62d6cc42977067b98417812b7fab48ac949506
Michał Dulko <michal.dulko@gmail.com>
2006-05-14 11:26:05 +0000

    -Podbicie wersji

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-#define FILEVER  1,5,1,0
-#define PRODUCTVER  1,5,1,0
-#define STRFILEVER  "1.5.1.0"
-#define STRPRODUCTVER  "1.5.1.0"
+#define FILEVER  1,5,2,0
+#define PRODUCTVER  1,5,2,0
+#define STRFILEVER  "1.5.2.0"
+#define STRPRODUCTVER  "1.5.2.0"
