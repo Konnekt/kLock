58d75cb7ace238bfa91f8c1e230ea3fcf9bb160e
Michał Dulko <michal.dulko@gmail.com>
2006-10-28 09:44:21 +0000

    - Dodanie stdafx.h i drobna zmiana w projekcie

---
+++ stdafx.h
@@ -0,0 +1,27 @@
+/*
+ * License - http://www.gnu.org/licenses/lgpl.txt
+ */
+
+#pragma once
+
+#define _WIN32_WINNT 0x0500
+
+//standardowe
+#include <string>
+#include <list>
+
+//windowsowe
+#include <windows.h>
+#include <commctrl.h>
+
+//konnektowe
+#include <konnekt/plug_export.h>
+#include <konnekt/ui.h>
+#include <konnekt/plug_func.h>
+#include <konnekt/ui_message_controls.h>
+#include <konnekt/tabletKa.h>
+#include <konnekt/kSound.h>
+#include <konnekt/lib.h>
+#include <konnekt/kAway2.h>
+#include <konnekt/KNotify.h>
+#include <konnekt/kMigacz.h>
